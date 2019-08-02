e1aa26a9a4f22a9077fc549da5a682b3a4473a4b
turovtsov <dturovtsov@marathonbet.ru>
2019-08-02 20:41:20 +0300

    exam2

---
+++ exam-004/exam-4.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello-4")
+	fmt.Println(len("привет мир!"))
+
+	i := 5
+	b := &i
+
+	*b = 6
+
+	fmt.Println(i)
+
+	sl := []int{0, 0, 0}
+	ChangeSlice(sl)
+	fmt.Println(sl)
+}
+
+// change slice
+func ChangeSlice(items []int) {
+	items[2] = 1
+}
